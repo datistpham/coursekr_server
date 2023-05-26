35166ea59584c3d58665f001a2402b8bd3341111
datistpham <78075406+datistpham@users.noreply.github.com>
2023-05-26 10:55:14 +0700

    Update connect.js

--- database/connect.js
+++ database/connect.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
   const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'courseweb'
+    host: '151.106.124.151',
+    user: '	u291392387_root17',
+    password: 'Chubeanxoi1!',
+    database: 'u291392387_coursewebkr'
   });
   
 
